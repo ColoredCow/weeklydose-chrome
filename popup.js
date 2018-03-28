0f85b8afe500ed073ff92f48a3393a675a0e80d3
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-03-28 15:05:46 +0530

    more changes

--- popup.js
+++ popup.js
@@ -1,28 +1,20 @@
 function submitReadingForm(form) {
-  chrome.storage.sync.set({'cc_reader': $('#reader').val()}, function(){});
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    var readingData = {
-      "topic" : $('#topic').val(),
-      "url" : tabs[0].url,
-      "recommended_by" : $('#reader').val(),
-    };
-    $.ajax({
-      url: 'https://weeklydose.dev/api/reading-items/new',
-      method: 'POST',
-      data: readingData,
-      success: function(res) {
-        $('#submitReadingItem').hide();
-        $('#submittedReadingItem').show();
-      },
-      error: function(err) {
-        alert('There was an error submitting! Please refresh the page and try again.');
-      }
-    });
+  chrome.storage.sync.set({'cc_reader': $('#recommended_by').val()}, function(){});
+  $.ajax({
+    url: 'https://ccweeklydose.dev/api/reading-items/new',
+    method: 'POST',
+    data: form.serialize(),
+    success: function(res) {
+      $('#submitReadingItem').hide();
+      $('#submittedReadingItem').show();
+    },
+    error: function(err) {
+      alert('There was an error submitting! Please refresh the page and try again.');
+    }
   });
 }
 
-function sendReadingLog() {
+function sendReadingLog(form) {
   var form = $('#formAddReadingLog');
   if (! form[0].checkValidity()) {
     form[0].reportValidity();
@@ -34,8 +26,13 @@ function sendReadingLog() {
 document.addEventListener('DOMContentLoaded', function() {
   chrome.storage.sync.get(['cc_reader'], function(items) {
     if (typeof items.cc_reader !== undefined) {
-      $('#reader').val(items.cc_reader);
+      $('#recommended_by').val(items.cc_reader);
     }
+  });
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    $('#share-link').text(url);
+    $('#url').val(url);
   });
   document.getElementById('submitReadingItem').addEventListener('click', sendReadingLog, false);
 });
