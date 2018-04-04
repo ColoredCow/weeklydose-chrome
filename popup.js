dd35cf42d712d1dd405c5127f702804c0831519b
Vaibhav Rathore <vaibhav@coloredcow.in>
2018-04-04 12:40:58 +0530

    validation checks

--- popup.js
+++ popup.js
@@ -1,17 +1,23 @@
 function submitReadingForm(form) {
   $('#submit_weeklydose').prop('disabled', 'disabled').addClass('disabled');
-  $.ajax({
-    // url: 'https://weeklydose.coloredcow.com/api/weeklydose',
-    url: 'https://ccweeklydose.dev/api/weeklydose',
-    method: 'POST',
-    data: form.serialize(),
-    success: function(res) {
-      $('#submit_weeklydose').hide();
-      $('#submitted_weeklydose').show();
-    },
-    error: function(err) {
-      $('#submit_weeklydose').hide();
-      $('#error_weeklydose').show();
+  var service_url = '';
+  chrome.storage.sync.get(['weeklydose_service_url'], function(items) {
+    if (typeof items.weeklydose_service_url !== undefined) {
+      service_url = items.weeklydose_service_url + '/api/weeklydose';
+      console.log(service_url);
+      $.ajax({
+        url: service_url,
+        method: 'POST',
+        data: form.serialize(),
+        success: function(res) {
+          $('#submit_weeklydose').hide();
+          $('#submitted_weeklydose').show();
+        },
+        error: function(err) {
+          $('#submit_weeklydose').hide();
+          $('#error_weeklydose').show();
+        }
+      });
     }
   });
 }
@@ -35,6 +41,7 @@ function hideSettings() {
   $('form').addClass('hidden');
   $('#form_add_weeklydose, #show_settings').removeClass('hidden');
   $('#hide_settings').addClass('hidden');
+  $('#form_weeklydose_settings .response-msg').hide();
 }
 
 function saveSettings() {
@@ -50,17 +57,34 @@ function saveSettings() {
     'weeklydose_service_url': $('#settings_weeklydose_service_url').val()
   }, function(){});
   $('#saved_settings').show();
+  setConfigs();
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function setConfigs() {
   chrome.storage.sync.get(['username', 'weeklydose_service_url'], function(items) {
+
+    if (typeof items.username == undefined
+      || typeof items.weeklydose_service_url == undefined
+      || items.username == ''
+      || items.weeklydose_service_url == '') {
+      $('#submit_weeklydose').prop('disabled', 'disabled').addClass('disabled');
+      $('.error-banner').removeClass('hidden');
+    } else {
+      $('#submit_weeklydose').prop('disabled', false).removeClass('disabled');
+      $('.error-banner').addClass('hidden');
+    }
+
     if (typeof items.username !== undefined) {
       $('#recommended_by, #settings_recommended_by').val(items.username);
     }
-     if (typeof items.weeklydose_service_url !== undefined) {
+    if (typeof items.weeklydose_service_url !== undefined) {
       $('#settings_weeklydose_service_url').val(items.weeklydose_service_url);
     }
   });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setConfigs();
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     $('#share_link').text(url);
